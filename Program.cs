0483b314b352e47004a68c616faee2b53cf9f8ff
its julii <nanji828@gmail.com>
2022-11-09 18:26:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sum_Seconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine()); 
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            //calc
+            int tottalTime = firstTime + secondTime + thirdTime;
+            int minutes = tottalTime / 60;
+            int seconds = tottalTime % 60;
+            
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }    
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+        }
+    }
+}
